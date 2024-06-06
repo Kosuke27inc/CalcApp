7306ab61291fd3067a5263943505556ee8f538db
Kosuke Miyazaki <27inc.kosukemiyazaki@gmail.com>
2024-06-06 20:47:57 +0900

    Lesson4の課題「四則計算アプリ」完成しました！

--- app/src/main/java/jp/techacademy/kosuke/miyazaki/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/kosuke/miyazaki/calcapp/MainActivity.kt
@@ -1,11 +1,58 @@
 package jp.techacademy.kosuke.miyazaki.calcapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editText1: EditText
+    private lateinit var editText2: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editText1 = findViewById(R.id.editText_1)
+        editText2 = findViewById(R.id.editText_2)
+
+        findViewById<Button>(R.id.button_1).setOnClickListener { calculate("+") }
+        findViewById<Button>(R.id.button_2).setOnClickListener { calculate("-") }
+        findViewById<Button>(R.id.button_3).setOnClickListener { calculate("*") }
+        findViewById<Button>(R.id.button_4).setOnClickListener { calculate("/") }
+    }
+
+    private fun calculate(operator: String) {
+        val num1Str = editText1.text.toString()
+        val num2Str = editText2.text.toString()
+
+        if (num1Str.isEmpty() || num2Str.isEmpty()) {
+            Toast.makeText(this, "数値を入力してください", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val num1 = num1Str.toDouble()
+        val num2 = num2Str.toDouble()
+        var result = 0.0
+
+        when (operator) {
+            "+" -> result = num1 + num2
+            "-" -> result = num1 - num2
+            "*" -> result = num1 * num2
+            "/" -> {
+                if (num2 == 0.0) {
+                    Toast.makeText(this, "0で割ることはできません", Toast.LENGTH_SHORT).show()
+                    return
+                }
+                result = num1 / num2
+            }
+        }
+
+        val intent = Intent(this, MainActivity2::class.java)
+        intent.putExtra("result", result)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/jp/techacademy/kosuke/miyazaki/calcapp/MainActivity2.kt
@@ -0,0 +1,19 @@
+package jp.techacademy.kosuke.miyazaki.calcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val resultTextView: TextView = findViewById(R.id.resultTextView)
+
+        val result = intent.getDoubleExtra("result", 0.0)
+
+        resultTextView.text = result.toString()
+    }
+}
